1d8e86dba8c1292e3c238ed04049c7f07bc5d703
Getnet <getnetadugna25@gmail.com>
2022-10-20 04:43:45 -0400

    Time: 65 ms (70.75%), Space: 13.7 MB (97.45%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class ConcatCompare(int):
+    def __lt__(n1, n2):
+        return str(n1) + str(n2) > str(n2) + str(n1)
+        
+class Solution:    
+        
+    def largestNumber(self, nums: List[int]) -> str:
+        nums.sort(key=ConcatCompare)
+        return str(int("".join(map(str, nums))))
+            
